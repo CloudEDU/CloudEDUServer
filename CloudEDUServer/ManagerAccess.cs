f51e366204fb73aab8bdfac9bbc020a8e87e53fa
codinfox <lzh727@126.com>
2013-07-21 20:45:45 +0800

    add DAO functions, encapsulated query(linq) functions

---
+++ CloudEDUServer/ManagerAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CloudEDUServer
+{
+    public class ManagerAccess
+    {
+        public static MANAGER GetManagerByName(string name)
+        {
+            MANAGER manager = null;
+            using (CloudEDUEntities ctx = new CloudEDUEntities())
+            {
+                var returnValue = from mngr in ctx.MANAGERs
+                                  where mngr.NAME == name
+                                  select mngr;
+                manager = returnValue.FirstOrDefault<MANAGER>();
+            }
+            return manager;
+        }
+
+    }
+}
