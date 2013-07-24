bb98f37573a58808d2e0d8f99d6c0d72d50f551f
codinfox <lzh727@126.com>
2013-07-24 16:11:08 +0800

    加入课程、增加课程、计算用户年龄数据库过程导入

--- CloudEDUServer/CloudEDUModel.Context.cs
+++ CloudEDUServer/CloudEDUModel.Context.cs
@@ -12,6 +12,9 @@ namespace CloudEDUServer
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
+    using System.Data.Objects;
+    using System.Data.Objects.DataClasses;
+    using System.Linq;
     
     public partial class CloudEDUEntities : DbContext
     {
@@ -45,5 +48,51 @@ namespace CloudEDUServer
         public DbSet<COURSE_OK> COURSE_OK { get; set; }
         public DbSet<COURSE_PENDING> COURSE_PENDING { get; set; }
         public DbSet<COURSE_CANCEL> COURSE_CANCEL { get; set; }
+    
+        public virtual ObjectResult<CreateCourse_Result> CreateCourse(Nullable<int> teacher, string title, string intro, Nullable<int> category, Nullable<decimal> price, Nullable<int> pg, string icon_url)
+        {
+            var teacherParameter = teacher.HasValue ?
+                new ObjectParameter("teacher", teacher) :
+                new ObjectParameter("teacher", typeof(int));
+    
+            var titleParameter = title != null ?
+                new ObjectParameter("title", title) :
+                new ObjectParameter("title", typeof(string));
+    
+            var introParameter = intro != null ?
+                new ObjectParameter("intro", intro) :
+                new ObjectParameter("intro", typeof(string));
+    
+            var categoryParameter = category.HasValue ?
+                new ObjectParameter("category", category) :
+                new ObjectParameter("category", typeof(int));
+    
+            var priceParameter = price.HasValue ?
+                new ObjectParameter("price", price) :
+                new ObjectParameter("price", typeof(decimal));
+    
+            var pgParameter = pg.HasValue ?
+                new ObjectParameter("pg", pg) :
+                new ObjectParameter("pg", typeof(int));
+    
+            var icon_urlParameter = icon_url != null ?
+                new ObjectParameter("icon_url", icon_url) :
+                new ObjectParameter("icon_url", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<CreateCourse_Result>("CreateCourse", teacherParameter, titleParameter, introParameter, categoryParameter, priceParameter, pgParameter, icon_urlParameter);
+        }
+    
+        public virtual ObjectResult<EnrollCourse_Result> EnrollCourse(Nullable<int> course_id, Nullable<int> customer_id)
+        {
+            var course_idParameter = course_id.HasValue ?
+                new ObjectParameter("course_id", course_id) :
+                new ObjectParameter("course_id", typeof(int));
+    
+            var customer_idParameter = customer_id.HasValue ?
+                new ObjectParameter("customer_id", customer_id) :
+                new ObjectParameter("customer_id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<EnrollCourse_Result>("EnrollCourse", course_idParameter, customer_idParameter);
+        }
     }
 }
