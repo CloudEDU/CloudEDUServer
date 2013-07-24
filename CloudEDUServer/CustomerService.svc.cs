3965289b32807639e9b7883f1f49037c67444f58
liil444 <lr_5146@163.com>
2013-07-24 14:16:23 +0800

    Customer数据服务接口测试通过

---
+++ CloudEDUServer/CustomerService.svc.cs
@@ -0,0 +1,90 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace CloudEDUServer
+{
+    public class CustomerService : DataService<CloudEDUEntities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
+            config.SetServiceOperationAccessRule("GetCustomersByName", ServiceOperationRights.All);
+            config.SetServiceOperationAccessRule("GetAllCustomers", ServiceOperationRights.All);
+            config.SetServiceOperationAccessRule("GetCustomersByNameAndPassword", ServiceOperationRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+        }
+
+        [WebGet]
+        public IQueryable<CUSTOMER> GetCustomersByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name", "You must provide a name!");
+            }
+            CloudEDUEntities ctx = new CloudEDUEntities();
+            try
+            {
+                var result = from crs in ctx.CUSTOMERs
+                             where crs.NAME == name
+                             select crs;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(string.Format(
+                    "An error occurred: {0}", ex.Message));
+            }
+        }
+
+        [WebGet]
+        public IQueryable<CUSTOMER> GetAllCustomers()
+        {
+            CloudEDUEntities ctx = new CloudEDUEntities();
+            try
+            {
+                var result = ctx.CUSTOMERs;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(string.Format(
+                    "An error occurred: {0}", ex.Message));
+            }
+        }
+
+        [WebGet]
+        public IQueryable<CUSTOMER> GetCustomersByNameAndPassword(string name, string password)
+        {
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentNullException("name", "You must provide a name!");
+            }
+            CloudEDUEntities ctx = new CloudEDUEntities();
+            try
+            {
+                var result = from crs in ctx.CUSTOMERs
+                             where crs.NAME == name && crs.PASSWORD == password
+                             select crs;
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(string.Format(
+                    "An error occurred: {0}", ex.Message));
+            }
+        }
+    }
+}
